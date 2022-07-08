dfee7c71d7c0c9cff402f1c473fcbe95714d6def
Milan Lesichkov <lesichkovm@gmail.com>
2022-07-08 06:38:53 +0000

    Updates

--- development/main.go
+++ development/main.go
@@ -1,24 +1,19 @@
 package main
 
 import (
-	"auth"
-	"auth/development/scribble"
-	"database/sql"
 	"errors"
 	"log"
 	"net/http"
 	"os"
 	"time"
 
-	"github.com/gouniverse/cms"
+	"github.com/gouniverse/auth/development/scribble"
+
+	"github.com/gouniverse/auth"
+
 	"github.com/gouniverse/utils"
-	// _ "github.com/go-sql-driver/mysql"
-	// _ "github.com/lib/pq"
-	// _ "github.com/mattn/go-sqlite3"
-	// _ "modernc.org/sqlite"
 )
 
-var myCms *cms.Cms
 var jsonStore *scribble.Driver
 
 func userRegister(username string, password string, first_name string, last_name string) error {
@@ -32,18 +27,6 @@ func userRegister(username string, password string, first_name string, last_name
 	if err != nil {
 		return err
 	}
-	// isOk, err := myCms.CacheStore.SetJSON(username, map[string]string{
-	// 	"username":   username,
-	// 	"password":   password,
-	// 	"first_name": first_name,
-	// 	"last_name":  last_name,
-	// }, 600)
-	// if err != nil {
-	// 	return err
-	// }
-	// if !isOk {
-	// 	return errors.New("unable to register")
-	// }
 	return nil
 }
 
@@ -55,10 +38,6 @@ func userLogin(username string, password string) (userID string, err error) {
 		return "not found err", err
 	}
 	log.Println(user)
-	// user, err := myCms.CacheStore.GetJSON(username, nil)
-	// if err != nil {
-	// 	return "not found err", err
-	// }
 	if user == nil {
 		return "not found", errors.New("unable to find user")
 	}
@@ -71,18 +50,6 @@ func userLogin(username string, password string) (userID string, err error) {
 }
 
 func userLogout(username string) error {
-	// slug := utils.Slugify(token, rune('_'))
-	// err := jsonStore.Write("tokens", slug, userID)
-	// if err != nil {
-	// 	return err
-	// }
-	// isOk, err := myCms.SessionStore.Set(token, userID, 600)
-	// if err != nil {
-	// 	return err
-	// }
-	// if !isOk {
-	// 	return errors.New("unable to store token")
-	// }
 	return nil
 }
 
@@ -92,13 +59,6 @@ func userStoreToken(token string, userID string) error {
 	if err != nil {
 		return err
 	}
-	// isOk, err := myCms.SessionStore.Set(token, userID, 600)
-	// if err != nil {
-	// 	return err
-	// }
-	// if !isOk {
-	// 	return errors.New("unable to store token")
-	// }
 	return nil
 }
 
@@ -108,7 +68,6 @@ func userFindByToken(token string) (userID string, err error) {
 	if err != nil {
 		return "not found err", err
 	}
-	// userID = myCms.SessionStore.Get(token, "")
 	return userID, nil
 }
 
@@ -125,19 +84,6 @@ func main() {
 		return
 	}
 
-	// db, err := mainDb(utils.Env("DB_DRIVER"), utils.Env("DB_HOST"), utils.Env("DB_PORT"), utils.Env("DB_DATABASE"), utils.Env("DB_USERNAME"), utils.Env("DB_PASSWORD"))
-	// defer db.Close()
-
-	// if err != nil {
-	// 	log.Panic("Database is NIL: " + err.Error())
-	// 	return
-	// }
-
-	// if db == nil {
-	// 	log.Panic("Database is NIL")
-	// 	return
-	// }
-
 	auth, err := auth.NewAuth(auth.Config{
 		Endpoint:             "/auth",
 		UrlRedirectOnSuccess: "/user/dashboard",
@@ -153,43 +99,15 @@ func main() {
 		log.Panicln(err.Error())
 	}
 
-	// log.Println("3. Initializing CMS...")
-	// myCms, err = cms.NewCms(cms.Config{
-	// 	DbInstance: db,
-	// 	// BlocksEnable:        true,
-	// 	CacheAutomigrate: true,
-	// 	CacheEnable:      true,
-	// 	// EntitiesAutomigrate: true,
-	// 	// LogsAutomigrate:     true,
-	// 	// LogsEnable:          true,
-	// 	// MenusEnable:         true,
-	// 	// PagesEnable:         true,
-	// 	// SettingsAutomigate:  true,
-	// 	// SettingsEnable:      true,
-	// 	SessionAutomigrate: true,
-	// 	SessionEnable:      true,
-	// 	// TemplatesEnable:     true,
-	// 	Prefix:           "cms",
-	// 	CustomEntityList: entityList(),
-	// })
-
-	// if err != nil {
-	// 	log.Panicln(err.Error())
-	// }
-
-	log.Println("4. Starting server on http://" + utils.Env("SERVER_HOST") + ":" + utils.Env("SERVER_PORT") + " ...")
-	log.Println("URL: http://" + utils.Env("APP_URL") + " ...")
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("<html>Index page. Login at: <a href='" + auth.LinkLogin() + "'>" + auth.LinkLogin() + "</a>"))
 	})
 	mux.HandleFunc("/auth/", auth.Router)
-	// mux.HandleFunc("/cms", myCms.Router)
-	// mux.HandleFunc("/user/dashboard", func(w http.ResponseWriter, r *http.Request) {
-	// 	auth.AuthMiddleware(Middleware)
-	// 	w.Write([]byte("User dashboard"))
-	// })
 	mux.Handle("/user/dashboard", auth.AuthMiddleware(messageHandler("<html>User page. Logout at: <a href='"+auth.LinkLogout()+"'>"+auth.LinkLogout()+"</a>")))
+
+	log.Println("4. Starting server on http://" + utils.Env("SERVER_HOST") + ":" + utils.Env("SERVER_PORT") + " ...")
+	log.Println("URL: http://" + utils.Env("APP_URL") + " ...")
 
 	srv := &http.Server{
 		Handler: mux,
@@ -217,34 +135,29 @@ func messageHandler(message string) http.Handler {
 	})
 }
 
-func mainDb(driverName string, dbHost string, dbPort string, dbName string, dbUser string, dbPass string) (*sql.DB, error) {
-	var db *sql.DB
-	var err error
-	if driverName == "sqlite" {
-		dsn := dbName + "?parseTime=true"
-		db, err = sql.Open("sqlite3", dsn)
-		// dsn := dbName
-		// db, err = sql.Open("sqlite", dsn)
-	}
-	if driverName == "mysql" {
-		dsn := dbUser + ":" + dbPass + "@tcp(" + dbHost + ":" + dbPort + ")/" + dbName + "?charset=utf8mb4&parseTime=True&loc=Local"
-		db, err = sql.Open("mysql", dsn)
-	}
-	if driverName == "postgres" {
-		dsn := "host=" + dbHost + " user=" + dbUser + " password=" + dbPass + " dbname=" + dbName + " port=" + dbPort + " sslmode=disable TimeZone=Europe/London"
-		db, err = sql.Open("postgres", dsn)
-	}
-	if err != nil {
-		return nil, err
-	}
-	if db == nil {
-		return nil, errors.New("database for driver " + driverName + " could not be intialized")
-	}
+// func mainDb(driverName string, dbHost string, dbPort string, dbName string, dbUser string, dbPass string) (*sql.DB, error) {
+// 	var db *sql.DB
+// 	var err error
+// 	if driverName == "sqlite" {
+// 		dsn := dbName + "?parseTime=true"
+// 		db, err = sql.Open("sqlite3", dsn)
+// 		// dsn := dbName
+// 		// db, err = sql.Open("sqlite", dsn)
+// 	}
+// 	if driverName == "mysql" {
+// 		dsn := dbUser + ":" + dbPass + "@tcp(" + dbHost + ":" + dbPort + ")/" + dbName + "?charset=utf8mb4&parseTime=True&loc=Local"
+// 		db, err = sql.Open("mysql", dsn)
+// 	}
+// 	if driverName == "postgres" {
+// 		dsn := "host=" + dbHost + " user=" + dbUser + " password=" + dbPass + " dbname=" + dbName + " port=" + dbPort + " sslmode=disable TimeZone=Europe/London"
+// 		db, err = sql.Open("postgres", dsn)
+// 	}
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	if db == nil {
+// 		return nil, errors.New("database for driver " + driverName + " could not be intialized")
+// 	}
 
-	return db, nil
-}
-
-func entityList() []cms.CustomEntityStructure {
-	list := []cms.CustomEntityStructure{}
-	return list
-}
+// 	return db, nil
+// }
--- go.mod
+++ go.mod
@@ -1,32 +1,17 @@
-module auth
+module github.com/gouniverse/auth
 
 go 1.18
 
-require (
-	github.com/gouniverse/cms v0.27.0
-	github.com/jcelliott/lumber v0.0.0-20160324203708-dd349441af25
-)
-
-require golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
+require github.com/jcelliott/lumber v0.0.0-20160324203708-dd349441af25
 
 require (
 	github.com/darkoatanasovski/htmltags v1.0.0 // indirect
-	github.com/doug-martin/goqu/v9 v9.18.0 // indirect
-	github.com/georgysavva/scany v1.0.0 // indirect
 	github.com/gouniverse/api v1.4.0
-	github.com/gouniverse/cachestore v0.11.0 // indirect
-	github.com/gouniverse/entitystore v0.48.0 // indirect
 	github.com/gouniverse/hb v1.33.0
-	github.com/gouniverse/icons v1.1.0 // indirect
-	github.com/gouniverse/logstore v1.2.0 // indirect
-	github.com/gouniverse/sessionstore v0.13.0 // indirect
-	github.com/gouniverse/settingstore v0.9.0 // indirect
-	github.com/gouniverse/uid v1.4.0 // indirect
 	github.com/gouniverse/utils v1.15.0
 	github.com/gouniverse/validator v0.11.0
 	github.com/joho/godotenv v1.4.0 // indirect
 	github.com/jordan-wright/email v4.0.1-0.20210109023952-943e75fe5223+incompatible // indirect
-	github.com/pkg/errors v0.9.1 // indirect
 	github.com/tdewolff/minify/v2 v2.11.11 // indirect
 	github.com/tdewolff/parse/v2 v2.6.0 // indirect
 	golang.org/x/net v0.0.0-20220630215102-69896b714898 // indirect
