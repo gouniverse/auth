598a296554d5ddb02522d0247d30b55544c8b143
Sinevia <info@sinevia.com>
2022-08-11 21:01:36 +0100

    Update

--- layout.go
+++ layout.go
@@ -3,8 +3,15 @@ package auth
 import "github.com/gouniverse/hb"
 
 func (a Auth) layout(content string) string {
+	font := hb.NewStyleURL("https://fonts.bunny.net/css?family=Nunito").ToHTML()
+	style := hb.NewStyle(`
+	html, body {
+		background: #f8fafc;
+		font-family: Nunito, sans-serif;;
+	}
+	`).ToHTML()
 	h := hb.NewSection().
-		Attr("style", "padding:80px 0px").
+		Attr("style", "padding:120px 0px").
 		AddChild(hb.NewHTML(content))
-	return h.ToHTML()
+	return font + style + h.ToHTML()
 }
